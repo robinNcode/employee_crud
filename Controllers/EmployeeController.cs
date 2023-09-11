0eb074583e1e85c9e23beab2319008ef9850bf66
robinNcode <msmrobin518@gmail.com>
2023-09-11 16:59:28 +0600

    `update`: Employee form

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace employee_crud.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
